45f2d2249a02468c8963665110e8e4b31fbb5bd5
Hugo_Navarro <tehspartanhugo@outlook.com>
2026-04-17 18:35:25 +0200

    Actualizado el fichero Ej3ContarPositivosTest.java

--- src/test/java/com/hugonavarro/ejerciciosjunit/Ej3ContarPositivosTest.java
+++ src/test/java/com/hugonavarro/ejerciciosjunit/Ej3ContarPositivosTest.java
@@ -11,4 +11,22 @@ class Ej3ContarPositivosTest {
         int[] datosActual = new int[]{Ej3ContarPositivos.contarPositivos(new int[]{})};
         assertArrayEquals(datosSolicitados, datosActual);
     }
+    @Test
+    void arrayNegativo() {
+        int[] datosSolicitados = new int[]{Ej3ContarPositivos.contarPositivos(new int[]{-1, -2, -3, -4, -5})};
+        int[] datosActual = new int[]{Ej3ContarPositivos.contarPositivos(new int[]{-1, -2, -3, -4, -5})};
+        assertArrayEquals(datosSolicitados, datosActual);
+    }
+    @Test
+    void arrayMezclado() {
+        int[] datosSolicitados = new int[]{Ej3ContarPositivos.contarPositivos(new int[]{0, 1, -2, -3})};
+        int[] datosActual = new int[]{Ej3ContarPositivos.contarPositivos(new int[]{0, 1, -2, -3})};
+        assertArrayEquals(datosSolicitados, datosActual);
+    }
+    @Test
+    void arrayPositivo() {
+        int[] datosSolicitados = new int[]{Ej3ContarPositivos.contarPositivos(new int[]{1, 2, 3, 4})};
+        int[] datosActual = new int[]{Ej3ContarPositivos.contarPositivos(new int[]{1, 2, 3, 4})};
+        assertArrayEquals(datosSolicitados, datosActual);
+    }
 }
